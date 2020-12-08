08b31a98812020b6962edeac634441580cc1e418
Italo Leal <itlcoorporativo@gmail.com>
2020-12-08 18:06:02 -0300

    Database produção

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,8 @@
-/*module.exports = {
+module.exports = {
     username: "rpvfiltcquscjb",
     password: "8b19fc10d90ace6d562c992636fa862e9bde6ec63ead84f3368a8c82f63dd613",
     database: "d2l81ase92jc78",
     host: "ec2-75-101-212-64.compute-1.amazonaws.com",
-    dialect: "postgres",
-    define: {
-      timestamps: true,
-      underscored: true
-    }
-  };*/
-
-
-  module.exports = {
-    username: "postgres",
-    password: "postgrespass",
-    database: "postgres",
-    host: "localhost",
     dialect: "postgres",
     define: {
       timestamps: true,
